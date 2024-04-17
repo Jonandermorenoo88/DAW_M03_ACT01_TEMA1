dbf74aee38b1408ea86c1a485ea6001c370566bb
jonandermorenoo88 <jaincera97@gmail.com>
2024-04-17 02:14:41 +0200

    creando la clase

--- DAW_M03_ACT01_TEMA1/src/ejercicio01/TiendaElectronica.java
+++ DAW_M03_ACT01_TEMA1/src/ejercicio01/TiendaElectronica.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ejercicio01;
+import java.util.Scanner;
 
 /**
  *
- * @author jonan
+ * @author Jon Ander Incera Moreno
  */
 public class TiendaElectronica {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int cTelefonosMoviles = 50;
+        int cOrdenadoresPortatiles = 30;
+        int cAuricularesBluetooth = 40;
+        
+        int ptelefonomovil = 300;
+        int pOrdenadoPortatil = 800;
+        int pAuricularesBluetooh =50;
+        
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("¿Cuantos Telefonos Moviles quieres comprar?");
+        int restoTelefono = cTelefonosMoviles - sc.nextInt();
+        System.out.println(restoTelefono);
+    }
     
 }
